fdddbc28d6fb5e165514e360bc0822b849b09876
Nemanja Kuzmanovic <kuzmaneca@gmail.com>
2020-10-24 13:16:12 +0200

    complete auth flow

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from '../models/userModel.js';
 import asyncHandler from 'express-async-handler';
+import generateToken from '../utils/generateToken.js';
 
 /**
  * @description   Auth user & get token
@@ -11,6 +12,68 @@ export const authUser = asyncHandler(async (req, res) => {
 
 	const user = await User.findOne({ email });
 
-  if(!user)
+	if (!user || !(await user.matchPassword(password))) {
+		res.status(401);
+		throw new Error('Invalid credentials');
+	}
 
+	res.status(200).json({
+		_id: user.id,
+		name: user.name,
+		email: user.email,
+		isAdmin: user.isAdmin,
+		token: generateToken(user._id),
+	});
+});
+
+/**
+ * @description   Register user
+ * @route         POST /api/users
+ * @access        Private
+ */
+export const registerUser = asyncHandler(async (req, res) => {
+	const { name, email, password } = req.body;
+
+	const userExists = await User.exists({ email });
+
+	if (userExists) {
+		res.status(400);
+		throw new Error('User already exists');
+	}
+
+	const user = await User.create({ name, email, password });
+
+	if (!user) {
+		res.status(400);
+		throw new Error('Invalid user data');
+	}
+
+	res.status(201).json({
+		_id: user.id,
+		name: user.name,
+		email: user.email,
+		isAdmin: user.isAdmin,
+		token: generateToken(user._id),
+	});
+});
+
+/**
+ * @description   Get user profile
+ * @route         GET /api/users/profile
+ * @access        Private
+ */
+export const getUserProfile = asyncHandler(async (req, res) => {
+	const user = await User.findById(req.user._id);
+
+	if (!user) {
+		res.status(404);
+		throw new Error('User Not Found');
+	}
+
+	res.status(200).json({
+		_id: user._id,
+		name: user.name,
+		email: user.email,
+		isAdmin: user.isAdmin,
+	});
 });
---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+import asyncHandler from 'express-async-handler';
+import jwt from 'jsonwebtoken';
+import User from '../models/userModel.js';
+
+export const protect = asyncHandler(async (req, res, next) => {
+	if (
+		!req.headers.authorization ||
+		!req.headers.authorization.startsWith('Bearer')
+	) {
+		res.status(401);
+		throw new Error('Not authorized. No token!');
+	}
+
+	try {
+		const token = req.headers.authorization.split(' ')[1];
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+		req.user = await User.findById(decoded.id).select('-password');
+
+		next();
+	} catch (error) {
+		console.error(error);
+		res.status(401);
+		throw new Error('Not authorized. Invalid token!');
+	}
+});
